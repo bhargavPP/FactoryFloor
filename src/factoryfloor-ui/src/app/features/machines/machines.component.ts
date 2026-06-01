34c267c5c193eed2b6c079f46665606a9766ada9
Bhargav <bhargavpatel4319@gmail.com>
2026-06-01 17:11:30 -0400

    added machine service

--- src/factoryfloor-ui/src/app/features/machines/machines.component.ts
+++ src/factoryfloor-ui/src/app/features/machines/machines.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from '../../shared/material/material.module';
+
+import { Machine } from '../../models/machine.model';
+import { MachineService } from '../../services/machine.service';
 
 @Component({
   selector: 'app-machines',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    MaterialModule],
   templateUrl: './machines.component.html',
   styleUrl: './machines.component.scss'
 })
 export class MachinesComponent {
+  private machineService = inject(MachineService);
 
+  machines: Machine[] = [];
+
+  ngOnInit(): void {
+    this.loadMachines();
+  }
+
+  loadMachines(): void {
+    this.machineService.getMachines()
+      .subscribe({
+        next: (response) => {
+          this.machines = response;
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      });
+  }
 }
--- src/factoryfloor-ui/src/app/services/machine.service.ts
+++ src/factoryfloor-ui/src/app/services/machine.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Machine } from '../models/machine.model';
+
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MachineService {
+  private http=inject(HttpClient);
 
-  constructor() { }
+  private apiUrl = `${environment.apiUrl}/machines`;
+
+  getMachines(): Observable<Machine[]> {
+    return this.http.get<Machine[]>(this.apiUrl); 
+  }
+
 }
