8c2c62857776d283adadb654b679b4add746cf49
Bhargav <bhargavpatel4319@gmail.com>
2026-06-01 16:10:43 -0400

    Added Material Module

--- src/factoryfloor-ui/src/app/app.routes.ts
+++ src/factoryfloor-ui/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './layout/features/dashboard/dashboard.component';
+import { MachinesComponent } from './layout/features/machines/machines.component';
+import { TelemetryComponent } from './layout/features/telemetry/telemetry.component';
+import { AlertsComponent } from './layout/features/alerts/alerts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'machines', component: MachinesComponent },
+  { path: 'telemetry', component: TelemetryComponent },
+  { path: 'alerts', component: AlertsComponent }
+];
