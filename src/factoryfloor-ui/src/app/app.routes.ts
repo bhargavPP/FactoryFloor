70d2d3681abb3f4f496e1aea54d8773ad9ca64af
Bhargav <bhargavpatel4319@gmail.com>
2026-06-02 14:49:00 -0400

    added AUth Service

--- src/factoryfloor-ui/src/app/app.routes.ts
+++ src/factoryfloor-ui/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { MachinesComponent } from './features/machines/machines.component';
 import { TelemetryComponent } from './features/telemetry/telemetry.component';
 import { AlertsComponent } from './features/alerts/alerts.component';
+import { LoginComponent } from './features/login/login.component';
 
 export const routes: Routes = [{
   path: '',
@@ -12,7 +13,11 @@ export const routes: Routes = [{
     { path: '', component: DashboardComponent },
     { path: 'machines', component: MachinesComponent },
     { path: 'telemetry', component: TelemetryComponent },
-    { path: 'alerts', component: AlertsComponent }
+    { path: 'alerts', component: AlertsComponent },
+    {
+      path: 'login',
+      component: LoginComponent
+    },
   ]
 }
 ];
